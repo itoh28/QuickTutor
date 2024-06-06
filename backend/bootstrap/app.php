e0f620e69fc4852aa7fcce138f579d23b98536ff
itoh28 <itoyusuke1994@gmail.com>
2024-06-06 19:45:23 +0900

    Add TrustProxies.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
