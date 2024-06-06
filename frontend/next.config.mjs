351c5f973c5f03b7434ecef8c108d7e2ff1128b9
itoh28 <itoyusuke1994@gmail.com>
2024-06-06 21:07:09 +0900

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+
   async rewrites() {
     return [
       {
