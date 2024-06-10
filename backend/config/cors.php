e78f261b70d167da5c6bd9e3dc5fcb20742dc4d1
itoh28 <itoyusuke1994@gmail.com>
2024-06-11 03:09:48 +0900

    Update php version

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,7 +2,10 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['http://localhost:3000', 'https://quicktutor.work', 'https://www.quicktutor.work'],
+    'allowed_methods' => [
+        'http://localhost:3000', 'http://quicktutor.work',  'http://www.quicktutor.work',
+        'https://quicktutor.work', 'https://www.quicktutor.work'
+    ],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
