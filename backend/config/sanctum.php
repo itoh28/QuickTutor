15ef7ea1c0e47508dcb433dc11903f3b13362806
itoh28 <itoyusuke1994@gmail.com>
2024-06-14 22:41:45 +0900

    Modify sanctum.php

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -17,8 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,
-        quicktutor.work,quicktutor.work:80,www.quicktutor.work,www.quicktutor.work:80',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,quicktutor.work,www.quicktutor.work',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
@@ -78,7 +77,7 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
     ],
 
 ];
