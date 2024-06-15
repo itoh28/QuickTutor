e23f795ba4aecc9e2ffd645c065c6d0f86e38e28
itoh28 <itoyusuke1994@gmail.com>
2024-06-16 02:18:59 +0900

    Change from SPA authentication to API token authentication

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     'allowed_origins' => ['https://quicktutor.work', 'https://www.quicktutor.work'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-CSRF-TOKEN', 'X-XSRF-TOKEN'],
+    'allowed_headers' => ['Content-Type', 'Authorization'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -77,9 +77,9 @@ return [
     */
 
     'middleware' => [
-        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        'authenticate_session' => null,
+        'encrypt_cookies' => null,
+        'validate_csrf_token' => null,
     ],
 
 ];
