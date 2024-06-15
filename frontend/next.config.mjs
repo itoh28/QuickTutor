d706d656ad8f619dab201d88b7245338f5c2f35a
itoh28 <itoyusuke1994@gmail.com>
2024-06-15 13:37:42 +0900

    Modify cors.php

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
