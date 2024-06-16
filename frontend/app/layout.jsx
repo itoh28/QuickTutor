9985183785e71cd9bc25b665d6ec67ab67713bb7
itoh28 <itoyusuke1994@gmail.com>
2024-06-16 18:00:08 +0900

    Modify layout.jsx

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -15,10 +15,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
-      <body className={inter.className}>
-        <GetCsrfToken />
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
