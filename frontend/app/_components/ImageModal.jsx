5fc519abfd12ba0d77d9aae1dad3460dd8add878
itoh28 <itoyusuke1994@gmail.com>
2024-07-06 23:51:02 +0900

    Update ImageModal.jsx

--- frontend/app/_components/ImageModal.jsx
+++ frontend/app/_components/ImageModal.jsx
@@ -14,7 +14,7 @@ const ImageModal = ({ imageSrc, onClose }) => {
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
       onClick={handleBackgroundClick}
     >
-      <div className="relative max-w-7xl max-h-screen-full">
+      <div className="relative max-w-7xl max-h-full">
         <button
           onClick={onClose}
           className="absolute top-0 right-0 m-4 text-white text-2xl bg-black bg-opacity-75 rounded-full p-2 hover:bg-opacity-100 w-10 h-10 flex items-center justify-center"
